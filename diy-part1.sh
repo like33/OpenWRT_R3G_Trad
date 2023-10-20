c51fb0791ec89ef22df50e384e44addb170efdc1
李鹏辉 <92007593+like33@users.noreply.github.com>
2023-10-20 12:09:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,17 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+src-git packages https://github.com/coolsnowwolf/packages
+src-git luci https://github.com/coolsnowwolf/luci
+src-git routing https://github.com/coolsnowwolf/routing
+src-git telephony https://git.openwrt.org/feed/telephony.git
+src-git helloworld https://github.com/fw876/helloworld.git
+src-git oui https://github.com/zhaojh329/oui.git
+src-git video https://github.com/openwrt/video.git
+src-git targets https://github.com/openwrt/targets.git
+src-git oldpackages http://git.openwrt.org/packages.git
+src-link custom /usr/src/openwrt/custom-feed
+src-git lienol https://github.com/Lienol/openwrt-package
+src-git wifidog https://github.com/wifidog/wifidog-gateway.git
+src-git kenzo https://github.com/kenzok8/openwrt-packages
+src-git passwall https://github.com/xiaorouji/openwrt-passwall# 添加 OpenAppFilter 应用过滤
